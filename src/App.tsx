af307e53490aa9ce86e3884dd0b7ec5a5c4cb5ac
Laize Ferraz <laizeferraz@gmail.com>
2025-05-30 16:24:03 +1000

    add tailwind & shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-	return <h1>Hello World!!</h1>;
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">Hello World!!</h1>
+			<Button variant="destructive">Click me</Button>
+		</>
+	);
 }
 
 export default App;
