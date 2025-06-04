61f9b19312a848b6a4c4b6f661c1cbd7961e0dc8
Laize Ferraz <laizeferraz@gmail.com>
2025-06-04 17:21:52 +1000

    install packages to run unit tests & configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
@@ -10,5 +11,10 @@ export default defineConfig({
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
+	},
+	test: {
+		environment: "jsdom",
+		include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		globals: true,
 	},
 });
