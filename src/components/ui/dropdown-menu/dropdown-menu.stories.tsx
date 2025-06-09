3c79eaf65164b6458821d27ab3d06265837db4ad
Laize Ferraz <laizeferraz@gmail.com>
2025-06-09 18:58:35 +1000

    update story typeof

--- src/components/ui/dropdown-menu/dropdown-menu.stories.tsx
+++ src/components/ui/dropdown-menu/dropdown-menu.stories.tsx
@@ -25,7 +25,7 @@ const meta: Meta<typeof DropdownMenu> = {
 	},
 };
 export default meta;
-type Story = StoryObj<typeof DropdownMenu>;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
 	render: (args) => {
