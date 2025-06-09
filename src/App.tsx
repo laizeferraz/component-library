e1f34a09ec7c91516a2c51fdfcb0c3867ae19258
Laize Ferraz <laizeferraz@gmail.com>
2025-06-09 18:50:06 +1000

    add theme toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from "@/components/ui/theme/theme-provider";
 import { Button } from "./components/ui/button/button";
+import { ModeToggle } from "./components/ui/theme/theme-button";
 
 function App() {
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Hello World!!</h1>
-			<Button variant="destructive">Click me</Button>
-		</>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<div className="flex justify-end m-4">
+				<ModeToggle />
+			</div>
+			<div className="flex h-screen w-screen items-center justify-center bg-background">
+				<Button>Click me </Button>
+			</div>
+		</ThemeProvider>
 	);
 }
 
