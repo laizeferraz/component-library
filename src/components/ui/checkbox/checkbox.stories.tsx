ddce48377fa1d3e528feb9f7ce24b2992b4653fe
Laize Ferraz <laizeferraz@gmail.com>
2025-06-17 16:53:59 +1000

    add checkbox stories

---
+++ src/components/ui/checkbox/checkbox.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Label } from "../label/label";
+import { Checkbox } from "./checkbox";
+
+const meta: Meta<typeof Checkbox> = {
+	title: "Components/ui/Checkbox",
+	component: Checkbox,
+	tags: ["autodocs"],
+};
+export default meta;
+type Story = StoryObj<typeof Checkbox>;
+
+export const Default: Story = {
+	render: (args) => {
+		return (
+			<div className="flex items-center space-x-2">
+				<Checkbox id="terms" {...args} />
+				<Label htmlFor="terms">Accept terms and conditions</Label>
+			</div>
+		);
+	},
+};
